c802cf4b46bb9f1a8457eff98de245519dfec21e
sepehr <sepeehrz@gmail.com>
2025-10-22 13:49:24 +0330

    remove base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import {fileURLToPath, URL} from 'node:url';
 import vuetify from 'vite-plugin-vuetify';
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
   server: {
     port: 4575
   },
